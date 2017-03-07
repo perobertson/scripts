e69dab9445e07b10e875717d0bc6d746fded6456
Paul Robertson <t.paulrobertson@gmail.com>
2017-03-06 19:40:35 -0500

    setup mysql

--- fedora/setup_databases.sh
+++ fedora/setup_databases.sh
@@ -8,3 +8,30 @@ sudo systemctl start postgresql
 sudo systemctl enable postgresql
 sudo su --command="perl -p -i -e 's/host([\w :\/\.]*)ident/host\$1trust/g' /var/lib/pgsql/data/pg_hba.conf" --login postgres
 sudo su --command="psql --command='CREATE ROLE $(whoami) WITH SUPERUSER LOGIN;'" --login postgres
+
+# Setup MySql
+sudo dnf -y install mariadb \
+                    mariadb-server \
+                    expect
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+
+SECURE_MYSQL=$(expect -c "
+  set timeout 10
+  spawn mysql_secure_installation
+  expect \"Enter current password for root (enter for none):\"
+  send \"\r\"
+  expect \"Change the root password?\"
+  send \"n\r\"
+  expect \"Remove anonymous users?\"
+  send \"y\r\"
+  expect \"Disallow root login remotely?\"
+  send \"y\r\"
+  expect \"Remove test database and access to it?\"
+  send \"y\r\"
+  expect \"Reload privilege tables now?\"
+  send \"y\r\"
+  expect eof
+")
+echo "$SECURE_MYSQL"
+unset SECURE_MYSQL
