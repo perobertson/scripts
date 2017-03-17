ffdc56079e7a2b71c82952810d2e79322896aacb
Paul Robertson <t.paulrobertson@gmail.com>
2017-03-16 21:06:07 -0400

    install sublime on ubuntu

---
+++ ubuntu/setup_ides.sh
@@ -0,0 +1,3 @@
+# Install Sublime
+/usr/bin/curl -Lo "$HOME/Downloads/sublime-text_build-3126_amd64.deb" https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
+sudo apt install -y $HOME/Downloads/sublime-text_build-3126_amd64.deb
