ca12615c6ad59fc20a923d566a228df331fe25fb
Paul Robertson <t.paulrobertson@gmail.com>
2017-05-09 04:56:10 +0000

    set the default font

--- fedora/user_settings.sh
+++ fedora/user_settings.sh
@@ -1,5 +1,8 @@
 # Switch to zsh
 sudo usermod -s $(which zsh) $(whoami)
 
+# Change fonts
+gsettings set org.gnome.desktop.interface monospace-font-name 'Inconsolata Medium 9'
+
 # Clean up fonts
 echo "Xft.lcdfilter: lcddefault" > ~/.Xresources
