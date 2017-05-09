4fd962c8223bb32136e4aeb83f4d4ec08afda12b
Paul Robertson <t.paulrobertson@gmail.com>
2017-05-09 05:11:29 +0000

    Update user_settings.sh

--- fedora/user_settings.sh
+++ fedora/user_settings.sh
@@ -1,8 +1,12 @@
 # Switch to zsh
 sudo usermod -s $(which zsh) $(whoami)
 
-# Change fonts
+# Change settings
 gsettings set org.gnome.desktop.interface monospace-font-name 'Inconsolata Medium 9'
+gsettings set org.cinnamon.desktop.interface clock-show-seconds true
+gsettings set org.cinnamon.theme name 'Blue-Submarine'
+gsettings set org.cinnamon.desktop.interface gtk-theme 'Blue-Submarine'
+gsettings set org.cinnamon.desktop.interface icon-theme 'Mint-X-Aqua'
 
 # Clean up fonts
 echo "Xft.lcdfilter: lcddefault" > ~/.Xresources
