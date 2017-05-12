96e86ed13e573b6985655d85ca427c37076fa322
Paul Robertson <t.paulrobertson@gmail.com>
2017-05-12 04:14:05 +0000

    idempotent install of phantomjs

--- fedora/setup_dev_depends.sh
+++ fedora/setup_dev_depends.sh
@@ -58,6 +58,10 @@ sudo systemctl enable nmb
 sudo systemctl enable redis
 
 # Setup PhantomJS
-/usr/bin/curl -Lo "$HOME/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xvjf "$HOME/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" -C "$HOME/Applications"
-ln -s "$HOME/Applications/phantomjs-2.1.1-linux-x86_64/bin/phantomjs" "$HOME/bin/phantomjs"
+[[ ! -f "$HOME/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" ]] && /usr/bin/curl -Lo "$HOME/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+[[ ! -d "$HOME/Applications/phantomjs-2.1.1-linux-x86_64" ]] && tar xvjf "$HOME/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" -C "$HOME/Applications"
+if [[ -f "$HOME/bin/phantomjs" ]]; then
+  rm "$HOME/bin/phantomjs"
+else
+  ln -s "$HOME/Applications/phantomjs-2.1.1-linux-x86_64/bin/phantomjs" "$HOME/bin/phantomjs"
+fi
