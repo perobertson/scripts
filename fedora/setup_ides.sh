45bf1506cd266e05ba56cb315cb787312d7f46b2
Paul Robertson <t.paulrobertson@gmail.com>
2017-07-13 23:09:38 -0400

    preinstall a bunch of sublime packages and settings

--- fedora/setup_ides.sh
+++ fedora/setup_ides.sh
@@ -14,7 +14,25 @@ Icon=$(echo $HOME)/Applications/sublime_text_3/Icon/256x256/sublime-text.png
 Categories=Development;TextEditor;
 Type=Application" > "$HOME/.local/share/applications/sublime.desktop"
 mkdir -p "$HOME/.config/sublime-text-3/Packages/User"
-tee "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" <<EOF
+cat > "$HOME/.config/sublime-text-3/Packages/User/Flake8Lint.sublime-settings" <<EOF
+{
+  "popup": false,
+  "ignore": [
+    "E501", // line length
+    "I100", // import order
+    "I201", // newline before section of imports
+    "D100", // doc public module
+    "D101", // doc public class
+    "D102", // doc public method
+    "D103", // doc public function
+    "D105", // doc magic
+    "D200", // doc oneline
+    "D400", // doc end with period
+    "E265", // block comments start with #
+  ],
+}
+EOF
+cat > "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" <<EOF
 {
   "default_line_ending": "unix",
   "draw_white_space": "all",
@@ -42,6 +60,20 @@ tee "$HOME/.config/sublime-text-3/Packages/User/Preferences.sublime-settings" <<
   "line_padding_top": 1,
   "tab_size": 2,
   "translate_tabs_to_spaces": true,
-  "trim_trailing_white_space_on_save": true
+  "trim_trailing_white_space_on_save": true,
 }
 EOF
+cat > "$HOME/.config/sublime-text-3/Packages/User/Python.sublime-settings" <<EOF
+{
+  "tab_size": 4,
+}
+EOF
+cat > "$HOME/.config/sublime-text-3/Packages/User/python_fiximports.sublime-settings" <<EOF
+{
+  "split_import_statements": false,
+}
+EOF
+
+git clone https://github.com/srusskih/SublimeJEDI.git "$HOME/.config/sublime-text-3/Packages/Jedi - Python autocompletion"
+git clone https://github.com/Stibbons/python-fiximports.git "$HOME/.config/sublime-text-3/Packages/Python Fix Imports"
+git clone https://github.com/dreadatour/Flake8Lint.git "$HOME/.config/sublime-text-3/Packages/Python Flake8 Lint"
