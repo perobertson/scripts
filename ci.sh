0cdd48abd1c3a90867c45251680de981e7c32784
Paul Robertson <t.paulrobertson@gmail.com>
2017-10-20 09:29:15 -0400

    cant use a script here since it hasnt been downloaded yet

--- ci.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -z "$CI" ]; then
-  os=$(. /etc/os-release && echo $ID)
-  if [ "$os" = 'fedora' ]; then
-    dnf install -y sudo
-  else
-    apt-get update
-    apt-get install -y sudo
-  fi
-fi
--- setup.sh
+++ setup.sh
@@ -3,7 +3,15 @@
 # Display Commands
 set -x
 
-. ci.sh || exit 1
+if [ ! -z "$CI" ]; then
+  os=$(. /etc/os-release && echo $ID)
+  if [ "$os" = 'fedora' ]; then
+    dnf install -y sudo
+  else
+    apt-get update
+    apt-get install -y sudo
+  fi
+fi
 
 # Clear any previous sudo permission
 sudo -k
