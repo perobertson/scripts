d7a0fd80b36604e35fa4c8333c749f9cd9e44fc3
Paul Robertson <t.paulrobertson@gmail.com>
2018-01-06 06:55:36 -0500

    test for phantomjs to be installed

--- fedora/setup_dev_depends.sh
+++ fedora/setup_dev_depends.sh
@@ -70,9 +70,9 @@ sudo systemctl enable redis
 [[ ! -d "$HOME/Applications/phantomjs-2.1.1-linux-x86_64" ]] && tar xvjf "$HOME/Downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2" -C "$HOME/Applications"
 if [[ -f "$HOME/bin/phantomjs" ]]; then
   rm "$HOME/bin/phantomjs"
-else
-  ln -s "$HOME/Applications/phantomjs-2.1.1-linux-x86_64/bin/phantomjs" "$HOME/bin/phantomjs"
 fi
+ln -s "$HOME/Applications/phantomjs-2.1.1-linux-x86_64/bin/phantomjs" "$HOME/bin/phantomjs"
+[[ "$($HOME/bin/phantomjs --version)" == '2.1.1' ]] || exit 1
 
 # Setup Heroku CLI
 [[ ! -f "$HOME/Downloads/heroku.tar.gz" ]] && wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O "$HOME/Downloads/heroku.tar.gz"
