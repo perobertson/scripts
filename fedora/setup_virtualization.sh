3e744802b7c2ee8d5fe2a48b5215634b8f7adb0d
Paul Robertson <t.paulrobertson@gmail.com>
2018-01-23 07:46:43 -0500

    enable docker at system boot

--- fedora/setup_virtualization.sh
+++ fedora/setup_virtualization.sh
@@ -31,6 +31,8 @@ else
 
   sudo dnf -y install docker-ce || exit 1
   sudo usermod -a -G docker $(whoami)
+  sudo systemctl enable docker
+  sudo systemctl start docker
 
   sudo curl -L "https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
