dd8de34269a8796b297358018e478200d39133db
Paul Robertson <t.paulrobertson@gmail.com>
2018-03-18 11:00:34 -0400

    newer sublime linter

--- fedora/setup_ides.sh
+++ fedora/setup_ides.sh
@@ -6,90 +6,19 @@ mkdir -p "$HOME/.config/sublime-text-3/Packages/User"
 
 if [[ ! -f "$HOME/.config/sublime-text-3/Packages/User/SublimeLinter.sublime-settings" ]]; then
   cat > "$HOME/.config/sublime-text-3/Packages/User/SublimeLinter.sublime-settings" <<EOF
+// SublimeLinter Settings - User
 {
-    "user": {
-        "debug": false,
-        "delay": 0.25,
-        "error_color": "D02000",
-        "gutter_theme": "Packages/SublimeLinter/gutter-themes/Default/Default.gutter-theme",
-        "gutter_theme_excludes": [],
-        "lint_mode": "background",
-        "linters": {
-            "flake8": {
-                "@disable": false,
-                "args": [],
-                "builtins": "",
-                "excludes": [],
-                "executable": "$HOME/.local/bin/flake8",
-                "ignore": "",
-                "jobs": "",
-                "max-complexity": "",
-                "max-line-length": "240",
-                "select": "",
-                "show-code": true
-            }
-        },
-        "mark_style": "outline",
-        "no_column_highlights_line": false,
-        "passive_warnings": false,
-        "paths": {
-            "linux": [],
-            "osx": [],
-            "windows": []
-        },
-        "python_paths": {
-            "linux": [],
-            "osx": [],
-            "windows": []
-        },
-        "rc_search_limit": 3,
-        "shell_timeout": 10,
-        "show_errors_on_save": false,
-        "show_marks_in_minimap": true,
-        "syntax_map": {
-            "coffeescript (gulpfile)": "coffeescript",
-            "html (django)": "html",
-            "html (rails)": "html",
-            "html 5": "html",
-            "javascript (babel)": "javascript",
-            "javascript (eslint)": "javascript",
-            "javascript (gruntfile)": "javascript",
-            "javascript (gulpfile)": "javascript",
-            "javascript (postcss)": "javascript",
-            "javascript (stylelint)": "javascript",
-            "javascript (webpack)": "javascript",
-            "json (babel)": "json",
-            "json (bower)": "json",
-            "json (composer)": "json",
-            "json (eslint)": "json",
-            "json (npm)": "json",
-            "json (postcss)": "json",
-            "json (settings)": "json",
-            "json (stylelint)": "json",
-            "json (sublime)": "json",
-            "json (tern js)": "json",
-            "magicpython": "python",
-            "php": "html",
-            "python django": "python",
-            "pythonimproved": "python",
-            "xml (config)": "xml",
-            "xml (svg)": "xml",
-            "yaml (circleci)": "yaml",
-            "yaml (docker)": "yaml",
-            "yaml (eslint)": "yaml",
-            "yaml (lock)": "yaml",
-            "yaml (procfile)": "yaml",
-            "yaml (stylelint)": "yaml",
-            "yaml (yarn)": "yaml"
-        },
-        "tooltip_fontsize": "1rem",
-        "tooltip_theme": "Packages/SublimeLinter/tooltip-themes/Default/Default.tooltip-theme",
-        "tooltip_theme_excludes": [],
-        "tooltips": false,
-        "use_current_shell_path": false,
-        "warning_color": "DDB700",
-        "wrap_find": true
-    }
+  "paths": {
+    "linux": [
+      "~/.rbenv/shims",
+      "~/.rbenv/bin",
+      "~/bin",
+      "~/.bin",
+      "~/.local/bin"
+    ],
+    "osx": [],
+    "windows": []
+  },
 }
 EOF
 fi
@@ -164,9 +93,11 @@ if [[ ! -d "$HOME/.config/sublime-text-3/Packages/Python Fix Imports" ]]; then
 fi
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/SublimeLinter" ]]; then
   git clone https://github.com/SublimeLinter/SublimeLinter.git "$HOME/.config/sublime-text-3/Packages/SublimeLinter"
-  cd "$HOME/.config/sublime-text-3/Packages/SublimeLinter" && git checkout v3.10.8 && cd - || exit 1
+  # only need to enable this checkout when breaking changes are happening
+  # cd "$HOME/.config/sublime-text-3/Packages/SublimeLinter" && git checkout 4.1.1 && cd - || exit 1
 fi
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/SublimeLinter-flake8" ]]; then
   git clone https://github.com/SublimeLinter/SublimeLinter-flake8.git "$HOME/.config/sublime-text-3/Packages/SublimeLinter-flake8"
-  cd "$HOME/.config/sublime-text-3/Packages/SublimeLinter-flake8" && git checkout 3.0.2 && cd - || exit 1
+  # only need to enable this checkout when breaking changes are happening
+  # cd "$HOME/.config/sublime-text-3/Packages/SublimeLinter-flake8" && git checkout 4.0.1 && cd - || exit 1
 fi
