709b3902f04af08393990299eeb48330cbb5ace0
Paul Robertson <t.paulrobertson@gmail.com>
2018-03-18 10:15:52 -0400

    disable myysql install on ubuntu

--- ubuntu/setup_databases.sh
+++ ubuntu/setup_databases.sh
@@ -1,25 +1,25 @@
 # Setup MySql
-sudo apt-get install -y mysql-server \
-                        expect
-sudo systemctl start mysql
-sudo systemctl enable mysql
+# sudo apt-get install -y mysql-server \
+#                         expect
+# sudo systemctl start mysql
+# sudo systemctl enable mysql
 
-SECURE_MYSQL=$(expect -c "
-  set timeout 10
-  spawn mysql_secure_installation
-  expect \"Enter current password for root (enter for none):\"
-  send \"\r\"
-  expect \"Change the root password?\"
-  send \"n\r\"
-  expect \"Remove anonymous users?\"
-  send \"y\r\"
-  expect \"Disallow root login remotely?\"
-  send \"y\r\"
-  expect \"Remove test database and access to it?\"
-  send \"y\r\"
-  expect \"Reload privilege tables now?\"
-  send \"y\r\"
-  expect eof
-")
-echo "$SECURE_MYSQL"
-unset SECURE_MYSQL
+# SECURE_MYSQL=$(expect -c "
+#   set timeout 10
+#   spawn mysql_secure_installation
+#   expect \"Enter current password for root (enter for none):\"
+#   send \"\r\"
+#   expect \"Change the root password?\"
+#   send \"n\r\"
+#   expect \"Remove anonymous users?\"
+#   send \"y\r\"
+#   expect \"Disallow root login remotely?\"
+#   send \"y\r\"
+#   expect \"Remove test database and access to it?\"
+#   send \"y\r\"
+#   expect \"Reload privilege tables now?\"
+#   send \"y\r\"
+#   expect eof
+# ")
+# echo "$SECURE_MYSQL"
+# unset SECURE_MYSQL
