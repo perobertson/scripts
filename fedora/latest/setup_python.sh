a2b0d91f6afff962a72f801fa2c76d6682659cda
Paul Robertson <t.paulrobertson@gmail.com>
2018-05-24 07:53:39 -0400

    add in pyenv

---
+++ fedora/latest/setup_python.sh
@@ -0,0 +1,2 @@
+# Install pyenv
+git clone https://github.com/pyenv/pyenv.git "$HOME/.pyenv"
--- setup.sh
+++ setup.sh
@@ -83,6 +83,7 @@ fi
 . "${os}/${version}/setup_ides.sh"
 
 # These do not require root
+. "${os}/${version}/setup_python.sh"
 . "${os}/${version}/setup_ruby.sh"
 . "${os}/${version}/setup_dotfiles.sh"
 . "${os}/${version}/user_settings.sh"
