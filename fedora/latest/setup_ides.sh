e35b6e6b3fde80a11ce5bce9dcb0e5ccc9303e3b
Paul Robertson <t.paulrobertson@gmail.com>
2019-03-03 16:14:26 +0000

    add StGitlab plugin

--- fedora/latest/setup_ides.sh
+++ fedora/latest/setup_ides.sh
@@ -31,6 +31,9 @@ fi
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/json_reindent" ]]; then
     git clone https://github.com/ThomasKliszowski/json_reindent.git "$HOME/.config/sublime-text-3/Packages/json_reindent"
 fi
+if [[ ! -d "$HOME/.config/sublime-text-3/Packages/StGitlab" ]]; then
+    git clone https://github.com/tosher/StGitlab.git "$HOME/.config/sublime-text-3/Packages/StGitlab"
+fi
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/sublime-nginx" ]]; then
     git clone https://github.com/brandonwamboldt/sublime-nginx.git "$HOME/.config/sublime-text-3/Packages/sublime-nginx"
 fi
