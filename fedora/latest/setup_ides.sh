7ec4f28d4bc404826622382cdaf0203e3510ed08
Paul Robertson <t.paulrobertson@gmail.com>
2019-03-18 14:31:54 +0000

    remove git gutter; now part of sublime

--- fedora/latest/setup_ides.sh
+++ fedora/latest/setup_ides.sh
@@ -19,9 +19,6 @@ fi
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/FileIcons" ]]; then
     git clone https://github.com/braver/FileIcons.git "$HOME/.config/sublime-text-3/Packages/FileIcons"
 fi
-if [[ ! -d "$HOME/.config/sublime-text-3/Packages/GitGutter" ]]; then
-    git clone https://github.com/jisaacks/GitGutter.git "$HOME/.config/sublime-text-3/Packages/GitGutter"
-fi
 if [[ ! -d "$HOME/.config/sublime-text-3/Packages/Jedi - Python autocompletion" ]]; then
     git clone https://github.com/srusskih/SublimeJEDI.git "$HOME/.config/sublime-text-3/Packages/Jedi - Python autocompletion"
 fi
