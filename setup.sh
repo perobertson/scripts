848f711e0671ffaa6d7ea44e21f85effed6e4e23
Paul Robertson <t.paulrobertson@gmail.com>
2020-01-25 18:45:49 +0000

    cleanup ansible script output

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-
-# Display Commands
-set -x
-
 if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
     # Bash 4.4, Zsh
     # e: Exit on non 0 exit codes
@@ -14,8 +10,7 @@ else
     set -eo pipefail
 fi
 # null the glob when nothing matches
-# TODO: figure out why this breaks `rake`
-# shopt -s nullglob
+shopt -s nullglob
 
 # enable recursive globbing
 shopt -s globstar
@@ -27,46 +22,44 @@ PATH="$HOME/.local/bin:$HOME/bin:$PATH"
 # Clear any previous sudo permission
 sudo -k
 
-# Check if we are in a CI environment
-if [ -z "${CI:-}" ]; then
-    # Check for root
-    [[ $(id -u) -eq 0 ]] && echo 'script must be run as a normal user' && exit 1
+# Check for root
+if [[ $(id -u) -eq 0 ]]; then
+    echo 'script must be run as a normal user'
+    exit 1
 fi
 
 # Set up app directories
-mkdir -p "$HOME/Applications"
-mkdir -p "$HOME/Downloads"
-mkdir -p "$HOME/bin"
-mkdir -p "$HOME/workspace"
+mkdir -pv "$HOME/Applications" "$HOME/Downloads" "$HOME/bin" "$HOME/workspace"
 
 # Check if git needs installed
-if [ ! -x "$(command -v git)" ]; then
-    if [ -x "$(command -v dnf)" ]; then
+if [[ ! -x "$(command -v git)" ]]; then
+    if [[ -x "$(command -v dnf)" ]]; then
         sudo dnf install -y git
     else
         sudo apt-get update
+        sudo apt-get install -y apt-transport-https
         sudo apt-get install -y git
     fi
 fi
 
 # Setup ssh
-if [ ! -d "$HOME/.ssh" ]; then
+if [[ ! -d "$HOME/.ssh" ]]; then
     mkdir "$HOME/.ssh"
     chmod 700 "$HOME/.ssh"
 fi
-if [ "$(grep ^gitlab.com "$HOME/.ssh/known_hosts")" = '' ]; then
+if [[ "$(grep ^gitlab.com "$HOME/.ssh/known_hosts")" = '' ]]; then
     ssh-keyscan 'gitlab.com' >> "$HOME/.ssh/known_hosts" 2>/dev/null
 fi
-if [ "$(grep ^github.com "$HOME/.ssh/known_hosts")" = '' ]; then
+if [[ "$(grep ^github.com "$HOME/.ssh/known_hosts")" = '' ]]; then
     ssh-keyscan 'github.com' >> "$HOME/.ssh/known_hosts" 2>/dev/null
 fi
 
 # Fetch the scripts
-if [ ! -d "$HOME/Applications/scripts" ]; then
+if [[ ! -d "$HOME/Applications/scripts" ]]; then
     git clone https://gitlab.com/perobertson/scripts.git "$HOME/Applications/scripts"
     cd "$HOME/Applications/scripts"
 
-    if [ -n "${CI:-}" ]; then
+    if [[ -n "${CI:-}" ]]; then
         # if we are in CI, then checkout the specific version for testing
         git reset --hard "${CI_COMMIT_SHA}"
     fi
@@ -74,21 +67,20 @@ fi
 
 # Run the setup
 os="$(. /etc/os-release && echo "$ID")"
-if [ "$os" == 'fedora' ]; then
-    . "fedora/bootstrap.sh"
-elif [ "$os" == 'ubuntu' ]; then
-    . "ubuntu/bootstrap.sh"
-else
-    echo "$os not supported"
-    exit 1
-fi
+case "${os}" in
+    fedora) . "fedora/bootstrap.sh" ;;
+    ubuntu) . "ubuntu/bootstrap.sh" ;;
+    *)
+        echo "WARN: ${os} is not supported"
+        echo "Please submit an issue at https://gitlab.com/perobertson/scripts/issues"
+        echo "Attempting to run ansible-playbook anyways..."
+    ;;
+esac
 
-set +x
-
-ansible-playbook -v --diff setup.yml
-if [ -z "${CI:-}" ]; then
+ansible-playbook -v setup.yml
+if [[ -z "${CI:-}" ]]; then
     # only start the services when outside of docker
-    ansible-playbook -v --diff -t systemd setup.yml
+    ansible-playbook -v -t systemd setup.yml
 fi
 
 echo ''
