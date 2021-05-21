d5e3fc1a84d478da541dce1f3e68beca0855457b
Paul Robertson <me@perobertson.com>
2021-05-21 12:52:18 -0400

    all: upgrade to ansible 4.x
    all: always lint with the latest linter

---
+++ bootstrap/install_ansible.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# use python3 as the default
+pip3 install --user --upgrade 'pip>=21.0'
+# force the shell to forget all remembered locations
+hash -r
+# update the packages used in the install process
+pip3 install --user --upgrade setuptools wheel
+# Remove old ansible base package if it is installed
+pip3 uninstall -y ansible-base
+# Make sure ansible is using python3
+pip3 install --user --upgrade 'ansible>=4.0.0,<5.0.0'
