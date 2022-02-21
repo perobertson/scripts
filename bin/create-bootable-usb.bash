39b0056452a8a64d40f49d4d6ac245611dca8e21
Paul Robertson <me@perobertson.com>
2022-02-21 14:27:36 -0500

    all: abort on trying to create windows bootable usbs

--- bin/create-bootable-usb.bash
+++ bin/create-bootable-usb.bash
@@ -11,6 +11,19 @@ set -euo pipefail
 
 ISO="${1:? path to ISO is required}"
 DISK="${2:? block device to write to is required}"
+
+if basename "${ISO}" | grep --ignore-case '^win.*\.iso$' >/dev/null; then
+    echo "ERROR: this script does not work with Windows ISOs."
+    echo "TLDR: Use rufus on Windows to make one."
+    echo "---"
+    echo "See: https://itsfoss.com/bootable-windows-usb-linux/"
+    echo "TLDR: Partition the drive using GPT."
+    # ExFAT or NTFT must be used because there are files larger than 4G
+    echo "TLDR: Create ExFAT or NTFS filesystem on the new partition."
+    echo "TLDR: Copy the contents of the ISO to the new filesystem."
+    exit 1
+fi
+
 ISO_SIZE="$(/usr/bin/du -b "${ISO}" | cut -f1)"
 
 available=$(lsblk -o NAME,TYPE --noheadings -l | grep '[[:space:]]disk' | cut -f1 -d' ')
