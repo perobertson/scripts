6c1c1fa1f5d736a4693b538dc9e18cdca48cbaad
Paul Robertson <me@perobertson.com>
2022-06-22 22:29:40 -0400

    all: update to ansible 6.x

--- bootstrap/install_ansible.bash
+++ bootstrap/install_ansible.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # use python3 as the default
-pip3 install --user --upgrade 'pip>=21.0'
+pip3 install --user --upgrade 'pip>=22.0'
 # force the shell to forget all remembered locations
 hash -r
 # update the packages used in the install process
@@ -10,4 +10,4 @@ pip3 install --user --upgrade setuptools wheel
 # Remove old ansible base package if it is installed
 pip3 uninstall -y ansible-base
 # Make sure ansible is using python3
-pip3 install --user --upgrade 'ansible>=5.0.0,<6.0.0'
+pip3 install --user --upgrade 'ansible>=6.0.0,<7.0.0'
