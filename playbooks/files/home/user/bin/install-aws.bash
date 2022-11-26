504e20b639ee55f20014e864ed96f85203eab181
Paul Robertson <me@perobertson.com>
2022-11-26 10:15:12 -0500

    all: fetch the aws signing key before verifying

--- playbooks/files/home/user/bin/install-aws.bash
+++ playbooks/files/home/user/bin/install-aws.bash
@@ -9,6 +9,12 @@ trap cleanup EXIT ERR
 
 cd /tmp
 
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html#getting-started-install-instructions
+# This is AWSs signing key
+if ! gpg -k FB5DB77FD5C118B80511ADA8A6310ACC4672475C > /dev/null; then
+    gpg --receive-keys FB5DB77FD5C118B80511ADA8A6310ACC4672475C
+fi
+
 /usr/bin/curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 /usr/bin/curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip.sig"
 /usr/bin/gpg --verify /tmp/awscli-exe-linux-x86_64.zip.sig /tmp/awscli-exe-linux-x86_64.zip
