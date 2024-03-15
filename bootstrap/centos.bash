be5ba59d84bec615c541bda277bbfa150274fa5c
Paul Robertson <me@perobertson.com>
2024-03-14 21:59:36 -0400

    use pipx on centos

--- bootstrap/centos.bash
+++ bootstrap/centos.bash
@@ -4,22 +4,10 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 sudo dnf -y install \
-    python3-dnf
-
-if [[ "$(source /etc/os-release && echo "$VERSION_ID")" = "8" ]]; then
-    sudo dnf -y install \
-        python39-devel \
-        python39-pip
-else
-    sudo dnf -y install \
-        python3-devel \
-        python3-pip
-fi
-
-# required to install dependencies of ansible
-sudo dnf -y install \
-    gcc \
-    libffi-devel
+    pipx \
+    python3-dnf \
+    python3-devel \
+    python3-pip
 
 # Setup ansible
 "${SCRIPT_DIR}/install_ansible.bash"
