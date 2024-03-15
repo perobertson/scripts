9c0b379c85f4a070b9e2b91cd000cc3b64937cb9
Paul Robertson <me@perobertson.com>
2024-03-14 22:29:02 -0400

    install pipx via pip for centos

--- bootstrap/centos.bash
+++ bootstrap/centos.bash
@@ -4,10 +4,18 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 sudo dnf -y install \
-    pipx \
     python3-dnf \
     python3-devel \
     python3-pip
+
+# use python3 as the default
+pip3 install --user --upgrade 'pip'
+# force the shell to forget all remembered locations
+hash -r
+# update the packages used in the install process
+pip3 install --user --upgrade setuptools wheel
+# install pipx
+pip3 install --user --upgrade pipx
 
 # Setup ansible
 "${SCRIPT_DIR}/install_ansible.bash"
